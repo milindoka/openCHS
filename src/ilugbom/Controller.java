f0c192daaf2c385807c8ab4a7265293438148d3f
milindoka <oak445@gmail.com>
2017-05-29 12:44:46 +0530

    First Commit - Basic MVC Design

---
+++ src/ilugbom/Controller.java
@@ -0,0 +1,63 @@
+package ilugbom;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controller {
+
+    private Model model;
+    private View view;
+    private ActionListener actionListener,printMCMbuttonListner;
+    
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+       // String path;
+     
+	     System.out.println(model.getJarPath()); ///set JAR path in model variable path;
+
+                          
+    }
+    
+    public void contol()
+    {        
+        actionListener = new ActionListener()
+        {
+              public void actionPerformed(ActionEvent actionEvent) {                  
+                  linkBtnAndLabel();
+              }
+        };                
+        view.getButton().addActionListener(actionListener);
+        
+        
+        printMCMbuttonListner = new ActionListener()
+        {
+              public void actionPerformed(ActionEvent actionEvent)
+              {                  
+                  printMCM();
+              }
+        };                
+        view.getButton().addActionListener(printMCMbuttonListner);
+        
+        
+        
+    }
+    
+    private void linkBtnAndLabel()
+    {
+        model.incX();                
+        view.setText(Integer.toString(model.getX()));
+        
+        
+    }
+    
+    private void printMCM()
+    {   
+        
+    }
+    
+    
+    
+    
+    
+    
+}
