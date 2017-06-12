470de16614ee11349c1b15a1dd7a814c6d74dcec
milindoka <oak444@gmail.com>
2017-06-12 09:36:47 +0530

    added ok cancel buttons with south panel

--- src/ilugbom/AgendaMinsDialog.java
+++ src/ilugbom/AgendaMinsDialog.java
@@ -3,28 +3,34 @@ package ilugbom;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.WindowConstants;
 
-public class AgendaMinsDialog extends JDialog {
+public class AgendaMinsDialog extends JDialog 
+{
 	  public AgendaMinsDialog( JFrame parent ) {
 	    super( parent );
-	    this.setTitle( "Roll Number Details..." );
+	    this.setTitle( "Add Agenda and Minutes Details..." );
 	    this.setLocationRelativeTo( null );
 	    this.setModal( true );
 	    this.setResizable( false );
 	    this.setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
 	    this.getContentPane().setLayout( new BorderLayout() );
-
+	    JPanel btnpanel=new JPanel();
+	    JButton okbutton=new JButton("OK");
+	    JButton cancelbutton=new JButton("Cancel");
+	    btnpanel.add(okbutton);btnpanel.add(cancelbutton);
 	    JTable shortcutKeysTable = getShortcutKeysTable();
 	  //  this.add( shortcutKeysTable, BorderLayout.CENTER );
 	    JScrollPane tpane=new JScrollPane(shortcutKeysTable);
 	    tpane.setPreferredSize(new Dimension( 800, 200) );
 	    this.add(tpane, BorderLayout.CENTER );
-       
+       this.add(btnpanel,BorderLayout.SOUTH);
 	    this.pack();
 	    this.setLocationRelativeTo(null);
 	    this.setVisible( true );
